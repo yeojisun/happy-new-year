acc4f0f17a4a0941ecbfd801cd9e0abf22256b2f
yeojis <ji.yeo@infranicsmail.com>
2021-12-28 15:01:04 +0900

    master

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import firebase from './firebase';
+//import firebase from './firebase';
 
 ReactDOM.render(
   <React.StrictMode>
