5d2648efccff3d5db114315f0aca96646d6979d2
yeojis <ji.yeo@infranicsmail.com>
2021-12-30 13:48:22 +0900

    test

--- src/App.js
+++ src/App.js
@@ -6,23 +6,22 @@ class App extends Component {
   render() {
 
      var getUserData = async () => {
-        var db = firebase.firestore();
-        db.settings({experimentalForceLongPolling: true, merge: true});
-        var docRef = db.collection("happynewyear").doc("users");
-        await docRef.get().then(function(doc) {
+      var db = firebase.firestore();
+      db.settings({experimentalForceLongPolling: true, merge: true});
+      var docRef = db.collection("happynewyear").doc("users");
+      try {
+          var doc = await docRef.get();
           if (doc.exists) {
               console.log(doc.data()); //see below for doc object
               return doc.data();
           } else {
               console.log("No such document!");
           }
-        }).catch(function(error) {
-            console.log("Error getting document:", error);
-        });
-      }
-        console.log(getUserData);
-    
-
+      } catch (error) {
+          console.log("Error getting document:", error);
+      };
+    }
+      console.log(getUserData);
 
     return (
       <div className="App">
