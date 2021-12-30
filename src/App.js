e7885ccb68eb5f4c2ccaa70c0744cd888fd931d3
yeojis <ji.yeo@infranicsmail.com>
2021-12-30 15:50:44 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ class App extends Component {
     const db = firebase.firestore();
     db.settings({experimentalForceLongPolling: true, merge: true});
 
-      db.collection("happynewyear").get().then((res)=>{
-        res.forEach((doc) =>{
-          console.log(doc.data())
-        })
-      });
+      db.collection("happynewyear").doc("greeting_img").get().then((doc) => {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
 
     return (
       <div className="App">
