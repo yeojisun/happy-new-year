3c617c58199065ad561a5f1bd067b36f6f198e1d
yeojis <ji.yeo@infranicsmail.com>
2021-12-30 10:53:02 +0900

    master

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import 'firebase/compat/firestore';
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATA_BASEURL,
+    //databaseURL: process.env.REACT_APP_FIREBASE_DATA_BASEURL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
