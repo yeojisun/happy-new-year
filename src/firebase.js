a6b8cb9d7ad824caf930c9e61951ec3cd0083e27
yeojis <ji.yeo@infranicsmail.com>
2021-12-30 17:48:12 +0900

    test

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,13 @@ import {initializeApp} from 'firebase/app'
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    //databaseURL: process.env.REACT_APP_FIREBASE_DATA_BASEURL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APPID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+    apiKey: "AIzaSyBNJvPesCSv7sMBvLFk3sdTNqx9Reud-cE",
+    authDomain: "happynewyear-2a527.firebaseapp.com",
+    projectId: "happynewyear-2a527",
+    storageBucket: "happynewyear-2a527.appspot.com",
+    messagingSenderId: "789134578125",
+    appId: "1:789134578125:web:62410aab9155f4ef3ff484",
+    measurementId: "G-35FH7SJYY3"
     // 인증키 
 };
 
