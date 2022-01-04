14490800425ce567e038682a158207413fa65962
yeojisun <duwltjs5921@gmail.com>
2022-01-05 00:19:06 +0900

    router test

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+function Header() {
+    return (
+        <header className='header'>
+            <strong>Header</strong>
+            <ul>
+                <li>
+                    <a href='/'>홈</a>
+                </li>
+                <li>
+                    <a href='/profile'>프로필</a>
+                </li>
+            </ul>
+        </header>);
+}
+export default Header;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Header from '../components/Header';
+function MainPage() {
+    return (
+        <> <Header /> <div>메인 페이지</div>; </>);
+}
+export default MainPage;
