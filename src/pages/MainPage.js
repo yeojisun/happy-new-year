d006583e965bbcad88951fac5bf55290e8021665
yeojisun <duwltjs5921@gmail.com>
2022-01-06 22:02:03 +0900

    오류 수정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Header';
+//import Header from '../components/Header';
 import Styled from "./Styled";
 import { Button } from '@mui/material';
 
