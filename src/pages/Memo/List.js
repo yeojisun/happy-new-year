a9919706974bc914b07d1c9fb55b24e30b8b4b95
yeojisun <duwltjs5921@gmail.com>
2022-01-09 22:05:40 +0900

    card list 컴포넌트 완

--- src/pages/Memo/List.js
+++ src/pages/Memo/List.js
@@ -1,4 +1,3 @@
-
 import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 
@@ -8,10 +7,17 @@ import { getFirestore, collection, query, getDocs } from "firebase/firestore";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import Typography from '@mui/material/Typography';
 
+import Slider from "react-slick";
 
+import Styled from "./Styled";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Image from './bg.jpg'; // Import using relative path
 function List() {
     const [cards, setCard] = useState([]);
     const fetchCard = async () => {
@@ -26,36 +32,50 @@ function List() {
     useEffect(() => {
         fetchCard();
     }, []);
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        rows: 3,
+        slidesToScroll: 3
+    };
+
+
+
+    const styles = {
+        paperContainer: {
+            backgroundImage: `url(${Image})`
+        }
+    };
 
     const { id } = useParams();
     return (
         <>
-            <div>{id}님의 덕담보따리</div>
+            <div>{id}님의 덕담보따1리</div>
+            <Styled>
+                <Slider {...settings}>
+                    {
+                        cards.map(c => {
+                            return (
+                                <Card sx={{ maxWidth: 200 }}>
+                                    <CardActionArea>
+                                        <CardContent style={styles.paperContainer}>
+                                            <Typography gutterBottom variant="h5" component="div">
+                                                {c.grt_title}
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary">
+                                                {c.grt_contents}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            )
+                        })
+                    }
+                </Slider>
+            </Styled>
 
-            {
-                cards.map(c => {
-                    return (
-                        <Card sx={{ maxWidth: 100 }}>
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    height="100"
-                                    image="/static/images/cards/contemplative-reptile.jpg"
-                                    alt="green iguana"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {c.grt_title}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {c.grt_contents}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    )
-                })
-            }
 
         </>
     );
---
+++ src/pages/Memo/Styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Styled = styled.div`
+
+.slick-next:before, .slick-prev:before {
+    color: #000;
+}
+`;
+
+export default Styled;
+
+
