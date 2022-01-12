af89e903870f7ce7136eabbecdab0b6cc3a0a6c6
reum <ost.jj1201@gmail.com>
2022-01-13 00:03:06 +0900

    logout 임시저장

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,41 +1,41 @@
-import Header from '../components/Header';
-import { useNavigate } from "react-router-dom";
+// import Header from '../components/Header';
+// import { useNavigate } from "react-router-dom";
 
-function ProfilePage() {
-  const history = useNavigate();
-  const user_id = sessionStorage.getItem("id");
-  const nickName = sessionStorage.getItem("nickName");
+// function ProfilePage() {
+//   const history = useNavigate();
+//   const user_id = sessionStorage.getItem("id");
+//   const nickName = sessionStorage.getItem("nickName");
 
-  const getProfile = async () => {
-    try {
-      // Kakao SDK API를 이용해 사용자 정보 획득
-      let data = await window.Kakao.API.request({
-        url: "/v2/user/me",
-      });
-      // 사용자 정보 저장
-      sessionStorage.setItem("id", data.id);
-      sessionStorage.setItem("nickName", data.properties.nickname);
+//   const getProfile = async () => {
+//     try {
+//       // Kakao SDK API를 이용해 사용자 정보 획득
+//       let data = await window.Kakao.API.request({
+//         url: "/v2/user/me",
+//       });
+//       // 사용자 정보 저장
+//       sessionStorage.setItem("id", data.id);
+//       sessionStorage.setItem("nickName", data.properties.nickname);
       
-    } catch (err) {
-      console.log(err);
-      alert("error: 로그인 조회");
-      history("/");
-    }
-  };
+//     } catch (err) {
+//       console.log(err);
+//       alert("error: 로그인 조회");
+//       history("/");
+//     }
+//   };
 
-    // 로그인 상태
-    if(user_id === null || user_id === '') {
-      // useEffect(() => {
-        getProfile();
-      // }, []);
-    }
+//     // 로그인 상태
+//     if(user_id === null || user_id === '') {
+//       // useEffect(() => {
+//         getProfile();
+//       // }, []);
+//     }
 
-  return (
-    <div>
-      <Header />  
-      <h2>{user_id}</h2>
-      <h2>{nickName}</h2>
-    </div>
-  );
-}
-export default ProfilePage;
+//   return (
+//     <div>
+//       <Header />  
+//       <h2>{user_id}</h2>
+//       <h2>{nickName}</h2>
+//     </div>
+//   );
+// }
+// export default ProfilePage;
