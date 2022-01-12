b03622366724eed5dc93788616d2d3f7b3bcc399
yeojis <ji.yeo@infranicsmail.com>
2022-01-12 09:06:45 +0900

    dialog 추가

--- src/pages/Memo/List.js
+++ src/pages/Memo/List.js
@@ -2,70 +2,81 @@ import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 
 import firebase from '../../firebase';
-import { getFirestore, collection, query, getDocs } from "firebase/firestore";
+import * as common from '../../CommonFunction';
+import { getFirestore, collection, query, orderBy, getDocs } from "firebase/firestore";
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import { CardActionArea, Typography, CardContent, Card } from '@mui/material';
 
 import Slider from "react-slick";
 
 import Styled from "./Styled";
+import View from "./View";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import Image from './bg.jpg'; // Import using relative path
 function List() {
     const [cards, setCard] = useState([]);
     const fetchCard = async () => {
         const database = getFirestore(firebase);  //정보가 올바르면 아래 파이어스토어 접근
-        const q = query(collection(database, "users/SUE3vmEn1CixjZiBBxZZ/greetings"))
+        const q = query(collection(database, "users/SUE3vmEn1CixjZiBBxZZ/greetings"), orderBy("grt_date"))
         getDocs(q).then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                setCard(cards => [...cards, doc.data()])
+                setCard(cards => [...cards, {...doc.data(), id: doc.id}]);
+
             })
         })
     }
+    
     useEffect(() => {
         fetchCard();
     }, []);
+
     const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        rows: 3,
-        slidesToScroll: 3
+        dots: true
+        , infinite: false
+        , speed: 500
+        , slidesPerRow: 3
+        , rows: 3
     };
 
 
-
-    const styles = {
-        paperContainer: {
-            backgroundImage: `url(${Image})`
-        }
-    };
 
     const { id } = useParams();
+
+    
+  const [open, setOpen] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState("test");
+
+  const handleClickOpen = (value) => {
+    setOpen(true);
+    setSelectedValue(value);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
+
     return (
         <>
-            <div>{id}님의 덕담보따1리</div>
+            <div>{id}님의 덕담보따리</div>
             <Styled>
                 <Slider {...settings}>
                     {
                         cards.map(c => {
                             return (
-                                <Card sx={{ maxWidth: 200 }}>
+                                <Card key={c.id} onClick={()=>{handleClickOpen(c.grt_title)}}>
                                     <CardActionArea>
-                                        <CardContent style={styles.paperContainer}>
+                                        <CardContent className="card_content" sx={{backgroundImage: `url('/assets/img/${c.grt_img}.jpg')`}}>
                                             <Typography gutterBottom variant="h5" component="div">
                                                 {c.grt_title}
                                             </Typography>
                                             <Typography variant="body2" color="text.secondary">
                                                 {c.grt_contents}
+                                            </Typography>
+                                            <Typography variant="caption" color="text.secondary">
+                                                {c.grt_date !== null ? common.timestamp(c.grt_date.toDate()) : null}
                                             </Typography>
                                         </CardContent>
                                     </CardActionArea>
@@ -77,6 +88,11 @@ function List() {
             </Styled>
 
 
+            <View
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+      />
         </>
     );
 }
--- src/pages/Memo/Styled.js
+++ src/pages/Memo/Styled.js
@@ -5,8 +5,19 @@ const Styled = styled.div`
 .slick-next:before, .slick-prev:before {
     color: #000;
 }
-`;
 
+.card_content {
+    
+    font-family: 'ChosunGs';
+    font-size: 40px;
+     }
+     @font-face {
+    font-family: 'ChosunGs';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.0/ChosunGs.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+`;
 export default Styled;
 
 
