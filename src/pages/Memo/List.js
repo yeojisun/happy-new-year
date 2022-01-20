a17b114a86dbed97537a81a9fde30a8394218676
reum <ost.jj1201@gmail.com>
2022-01-21 00:32:43 +0900

    View 진행중

--- src/pages/Memo/List.js
+++ src/pages/Memo/List.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation  } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 
 import firebase from '../../firebase';
@@ -16,20 +16,26 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function List() {
+    // 사용자 정보
+    const { id } = useParams();
+    const nickName = useLocation().state.nickName;
+    // 덕담 정보
     const [cards, setCard] = useState([]);
+
     const fetchCard = async () => {
         const database = getFirestore(firebase);  //정보가 올바르면 아래 파이어스토어 접근
         const q = query(collection(database, "users/SUE3vmEn1CixjZiBBxZZ/greetings"), orderBy("grt_date"))
+        // const q = query(collection(database, `users/${id}/greetings`), orderBy("grt_date"));
         getDocs(q).then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 setCard(cards => [...cards, {...doc.data(), id: doc.id}]);
-
             })
         })
     }
     
     useEffect(() => {
         fetchCard();
+        console.log("card>> ", cards);
     }, []);
 
     const settings = {
@@ -40,65 +46,89 @@ function List() {
         , rows: 3
 		, arrows: false
     };
-
-
-
-    const { id } = useParams();
-
     
-  const [open, setOpen] = React.useState(false);
-  const [selectedValue, setSelectedValue] = React.useState({});
+    // dialog state
+    const [open, setOpen] = React.useState(false);
+    const [selectedValue, setSelectedValue] = React.useState({});
+    
+    // open dialog
+    const handleClickOpen = (value, e) => {
+        console.log("dialog value>> ", value);
+        if(value != null && "NEW" === value.type) {
+            // 등록 View
+            console.log("등록");
+        
+        } else {
+            // 상세 View
+            console.log("상세");
+        
+        }
+        
+        setOpen(true);
+        setSelectedValue(value);
 
-  const handleClickOpen = (value) => {
-    setOpen(true);
-    setSelectedValue(value);
-  };
-
-  const handleClose = (value) => {
-    setOpen(false);
-    // setSelectedValue(value);
-  };
+        // return (
+        //     <View
+        //     selectedValue={selectedValue}
+        //     open={open}
+        //     onClose={handleClose}
+        // />
+        // );
+    };
+    // close dialog
+    const handleClose = (value) => {
+        setOpen(false);
+        // setSelectedValue(value);
+    };
 
     return (
         <>
         <Styled>
             <div className='frame'>
                 <main className='main-frame'>
-            <div>{id}님의 덕담보따리</div>
-                <Slider {...settings}>
-                    {
-                        cards.map(c => {
-                            return (
-                                <Card key={c.id} onClick={()=>{handleClickOpen(c)}}>
-                                    <CardActionArea>
-                                        <CardContent className="card_content" sx={{backgroundImage: `url('/assets/img/${c.grt_img}.jpg')`}}>
-                                            <Typography gutterBottom variant="h5" component="div">
-                                                {c.grt_title}
-                                            </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                {c.grt_contents}
-                                            </Typography>
-                                            <Typography variant="caption" color="text.secondary">
-                                                {c.grt_date !== null ? common.timestamp(c.grt_date.toDate()) : null}
-                                            </Typography>
-                                        </CardContent>
-                                    </CardActionArea>
-                                </Card>
-                            )
-                        })
-                    }
-                </Slider>
-        
-      </main>
-      </div>
-            </Styled>
+                    <div>{nickName}님의 덕담보따리</div>
+                    <div>
+                        <button onClick={() => handleClickOpen({type : 'NEW'})}>등록</button>
+                    </div>
+                
+                    <Slider {...settings}>
+                        {
+                            cards.map(c => {
+                                c.type = "VIEW";
 
-            <View
-        selectedValue={selectedValue}
-        open={open}
-        onClose={handleClose}
-      />
-        </>
+                                return (
+                                    <Card
+                                        key={c} 
+                                        onClick={() => handleClickOpen(c)} // TODO: 클릭 없이 페이지 랜더링 후 onclick 실행되는 문제 있음
+                                    >
+                                        <CardActionArea>
+                                            <CardContent className="card_content" sx={{backgroundImage: `url('/assets/img/${c.grt_img}.jpg')`}}>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                    {c.grt_title}
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    {c.grt_contents}
+                                                </Typography>
+                                                <Typography variant="caption" color="text.secondary">
+                                                    {c.grt_date !== null ? common.timestamp(c.grt_date.toDate()) : null}
+                                                </Typography>
+                                            </CardContent>
+                                        </CardActionArea>
+                                    </Card>
+                                );
+                            })
+                        }
+                    </Slider>
+                </main>
+            </div>
+        </Styled>
+        
+        <View
+            selectedValue={selectedValue}
+            open={open}
+            onClose={handleClose}
+        />
+    </>
     );
 }
 export default List;
--- src/pages/Memo/View.js
+++ src/pages/Memo/View.js
@@ -3,32 +3,43 @@ import * as common from '../../CommonFunction';
 import PropTypes from 'prop-types';
 
 function View(props) {
-    const { onClose, selectedValue, open } = props;
+  const { onClose, selectedValue, open } = props;
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
   
-    const handleClose = () => {
-      onClose(selectedValue);
-    };
+  const handleItemClick = (value) => {
+    onClose(value);
+  };
   
-    const handleItemClick = (value) => {
-      onClose(value);
-    };
-  
+  if(selectedValue != null && "NEW" !== selectedValue.type) {
     return (
       <Dialog onClose={handleClose} open={open} PaperProps={{
         style: {
           backgroundImage:`url("/assets/img/${selectedValue.grt_img}.jpg")`,  
         },
       }}>
-      <DialogTitle>{selectedValue.grt_title}</DialogTitle>
-        <DialogContent>{selectedValue.grt_contents}</DialogContent>
-        <DialogContent>{selectedValue.grt_date !== null ? common.timestamp(selectedValue.grt_date.toDate()) : null} {selectedValue.grt_user_id}로부터</DialogContent>
+        <DialogTitle>{selectedValue.type}</DialogTitle>
+        {/* <DialogContent>{selectedValue.grt_contents}</DialogContent>
+        <DialogContent>{selectedValue.grt_date !== null ? common.timestamp(selectedValue.grt_date.toDate()) : null} {selectedValue.grt_user_id}로부터</DialogContent> */}
         <Button onClick={()=> handleItemClick()}>닫기</Button>
       </Dialog>
     );
 
-    
+  } else {
+    return (
+      <Dialog onClose={handleClose} open={open} PaperProps={{
+        style: {
+          backgroundImage:`url("/assets/img/${selectedValue.grt_img}.jpg")`,  
+        },
+      }}>
+        <DialogTitle>{selectedValue.type}</DialogTitle>
+        <Button onClick={()=> handleItemClick()}>닫기</Button>
+      </Dialog>
+    );
   }
-  
+}
+
 export default View;
 
 View.propTypes = {
